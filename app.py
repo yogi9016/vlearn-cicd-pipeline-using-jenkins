6668fae41a0fa3aca0facb0964cae9339c17e55c
yogi9016 <yogeswaaran@appyhub.com>
2025-10-05 18:30:41 +0530

    test commit 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Welcome to the Flask CI/CD Demo!"})
+    return jsonify({"message": "Welcome to the Flask CI/CD Demo !"})
 
 @app.route("/health")
 def health():
